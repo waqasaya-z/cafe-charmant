4f02d169777368b8c104e76a4e238b456e96f8f9
waqasaya-z <waqasayaz@icloud.com>
2023-09-06 01:28:50 +0500

    customized Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,19 +3,18 @@ import { ShoppingCartIcon } from '@heroicons/react/24/outline'
 const Navbar = () => {
   return (
     <>
-    <header>
+    <header className='bg-stone-100 py-10'>
         <nav className='flex justify-between'>
             <div>
-           <a> CAFE </a>
+           <a href='/' className='text-xl font-bold uppercase tracking-widest'> CAFE CHARMANT </a>
             </div>
 
             <div>
-               <a> PRODUCTS </a> 
+               <a href='#' className='text-xs font-medium tracking-wider text-stone-700'> PRODUCTS </a> 
             </div>
 
-            <div className='flex flex-row'>
-            <button className='h-7 w-7' > <ShoppingCartIcon /> </button>
-            <button> SIGN IN </button>
+            <div className='flex items-center gap-x-2 pl-4'>
+            <button className='flex items-center gap-x-2 pl-4' > <ShoppingCartIcon className='h-7 w-7' /> </button>
             </div>
         </nav>
     </header>
